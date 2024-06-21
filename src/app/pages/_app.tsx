1ffd8e94208dacbc82ee75094faa60db26d35191
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-06-21 10:46:55 -0300

    new test projetct

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,5 +1,24 @@
-import type { AppProps } from 'next/app';
+import App, { AppContext, AppInitialProps, AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+type AppOwnProps = { example: string };
+
+export default function MyApp({
+    Component,
+    pageProps,
+    example,
+}: AppProps & AppOwnProps) {
+    return (
+        <>
+            <p>Data: {example}</p>
+            <Component {...pageProps} />
+        </>
+    );
+};
+
+MyApp.getInitialProps = async (
+    context: AppContext
+): Promise<AppOwnProps & AppInitialProps> => {
+    const ctx = await App.getInitialProps(context)
+
+    return { ...ctx, example: 'data' }
 };
