b82f77d74e9b6dbebbdf0c5565b869412e48763a
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-07-02 00:22:49 -0300

    update build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,8 @@
-import React from "react";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
-import enUS from 'date-fns/locale/en-US';
-import { eventsTest } from "@/components/events";
+import React from 'react';
+import AgendaPage from './andenda';
 
-const locales = {
-    'en-US': enUS
-};
-
-const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales
-});
-
-export default function AgendaPage() {
+export default function Page() {
     return (
-        <>
-            <Calendar
-                localizer={localizer}
-                events={eventsTest}
-                startAccessor="start"
-                endAccessor="end"
-                style={{ height: 500 }}
-            />
-        </>
+        <AgendaPage />
     );
 };
